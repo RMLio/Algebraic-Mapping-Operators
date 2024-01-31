7c309234559e27123c1c9123a4472973feaeec48
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2024-01-31 15:07:57 +0100

    Quick fix to escape regex characters in a string before applying a replaceAll

--- src/main/java/be/ugent/idlab/knows/amo/blocks/BGP.java
+++ src/main/java/be/ugent/idlab/knows/amo/blocks/BGP.java
@@ -1,14 +1,15 @@
 package be.ugent.idlab.knows.amo.blocks;
 
 import java.util.Collection;
+import java.util.regex.Pattern;
 
 /**
  * Basic Graph Pattern is a pattern to generate RDF triples by replacement of variables.
  */
 public class BGP {
 
-    private String pattern;
-    private Collection<String> variables;
+    private final String pattern;
+    private final Collection<String> variables;
 
     public BGP(String pattern, Collection<String> variables) {
         this.pattern = pattern;
@@ -19,7 +20,7 @@ public class BGP {
         String out = pattern;
         for (String variable : this.variables) {
             if (m.containsKey(variable)) {
-                out = out.replaceAll(variable, m.get(variable).toString());
+                out = out.replaceAll(Pattern.quote(variable), m.get(variable).toString());
             }
         }
 
