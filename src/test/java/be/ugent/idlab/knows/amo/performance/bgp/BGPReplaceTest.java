64291acef2dcab8ad2890258eac2e56e246f5b65
Jozef Jankaj <jozef.jankaj@ugent.be>
2024-02-05 10:42:23 +0100

    chore: add test for BGP replacement performance

---
+++ src/test/java/be/ugent/idlab/knows/amo/performance/bgp/BGPReplaceTest.java
@@ -0,0 +1,65 @@
+package be.ugent.idlab.knows.amo.performance.bgp;
+
+import be.ugent.idlab.knows.amo.blocks.SolutionMapping;
+import be.ugent.idlab.knows.amo.performance.bgp.classes.BGP;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BGPReplaceTest {
+
+    @Test
+    public void timingTestLargeBGP() {
+        StringBuilder patternBuilder = new StringBuilder();
+        StringBuilder solutionBuilder = new StringBuilder();
+
+        // generate a large pattern
+        int limit = 500;
+        Random random = new Random(5); // seeded to always get the same pattern and make results comparable
+        for (int i = 0; i < limit; i++) {
+            // randomly choose between ?a and ?b, place it in the array
+            if (random.nextBoolean()) {
+                patternBuilder.append("?a ");
+                solutionBuilder.append("small ");
+            } else {
+                patternBuilder.append("?b ");
+                solutionBuilder.append("Very Large Replacement String ");
+            }
+        }
+
+        String pattern = patternBuilder.toString();
+        String expected = solutionBuilder.toString();
+
+        SolutionMapping solMapping = new SolutionMapping(Map.of(
+                "?a", "small",
+                "?b", "Very Large Replacement String"
+        ));
+        BGP bgp = new BGP(pattern, Set.of("?a", "?b"));
+
+        long before, after;
+        before = System.nanoTime();
+        String out = bgp.apply(solMapping);
+        after = System.nanoTime();
+
+        assertEquals(expected, out);
+        System.out.printf("Time with RegEx: %dns\n", after - before);
+
+        before = System.nanoTime();
+        out = bgp.applyWithIndex(solMapping);
+        after = System.nanoTime();
+
+        assertEquals(expected, out);
+        System.out.printf("Time with index: %dns\n", after - before);
+
+        before = System.nanoTime();
+        out = bgp.applyWithStringUtils(solMapping);
+        after = System.nanoTime();
+
+        assertEquals(expected, out);
+        System.out.printf("Time with StringUtils::replaceEach : %dns\n", after - before);
+    }
+}
---
+++ src/test/java/be/ugent/idlab/knows/amo/performance/bgp/classes/BGP.java
@@ -0,0 +1,63 @@
+package be.ugent.idlab.knows.amo.performance.bgp.classes;
+
+import be.ugent.idlab.knows.amo.blocks.SolutionMapping;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collection;
+import java.util.regex.Pattern;
+
+/**
+ * Basic Graph Pattern is a pattern to generate RDF triples by replacement of variables.\
+ * Baseline implementation
+ */
+public class BGP {
+
+    private final String pattern;
+    private final Collection<String> variables;
+
+    public BGP(String pattern, Collection<String> variables) {
+        this.pattern = pattern;
+        this.variables = variables;
+    }
+
+    public String apply(SolutionMapping m) {
+        String out = pattern;
+        for (String variable : this.variables) {
+            if (m.containsKey(variable)) {
+                out = out.replaceAll(Pattern.quote(variable), m.get(variable).toString());
+            }
+        }
+
+        return out;
+    }
+
+    public String applyWithIndex(SolutionMapping m) {
+        String out = pattern;
+        for (String variable : this.variables) {
+            if (m.containsKey(variable)) {
+                int index = out.indexOf(variable);
+                while (index != -1) {
+                    out = out.replace(variable, m.get(variable).toString());
+                    index = out.indexOf(variable);
+                }
+            }
+        }
+
+        return out;
+    }
+
+    public String applyWithStringUtils(SolutionMapping m) {
+        String[] search = new String[m.size()];
+        String[] replacement = new String[m.size()];
+
+        int i = 0;
+        for (String key : m.keySet()) {
+            search[i] = key;
+            replacement[i] = m.get(key).toString();
+            i++;
+        }
+
+
+        return StringUtils.replaceEachRepeatedly(pattern, search, replacement);
+    }
+}
