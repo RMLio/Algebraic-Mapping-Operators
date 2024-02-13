9d07550b209a414bd8290f509ab4ce477521de6a
Jozef Jankaj <jozef.jankaj@ugent.be>
2024-02-13 10:27:41 +0100

    chore: rewrite BGP with Jena's Graph
    Reimplementation of BGP using Jena's Graph and Triples, in tandem with
    ARQ's Query for parsing from Strings.

--- src/main/java/be/ugent/idlab/knows/amo/blocks/BGP.java
+++ src/main/java/be/ugent/idlab/knows/amo/blocks/BGP.java
@@ -3,6 +3,9 @@ package be.ugent.idlab.knows.amo.blocks;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.sparql.graph.GraphFactory;
 
 import java.util.ArrayList;
@@ -13,11 +16,35 @@ import java.util.List;
  * Basic Graph Pattern is a pattern to generate RDF triples by replacement of variables.
  */
 public class BGP {
-    private List<Triple> triples = new ArrayList<>();
-    private List<Integer> subjectVariables = new ArrayList<>();
-    private List<Integer> objectVariables = new ArrayList<>();
+    private final List<Triple> triples = new ArrayList<>();
+    private final List<Integer> subjectVariables = new ArrayList<>();
+    private final List<Integer> objectVariables = new ArrayList<>();
+
+    /**
+     * Constructor for reading a BGP in from a String.
+     * @param pattern the BGP pattern
+     */
+    public BGP(String pattern) {
+        // turn into a CONSTRUCT query on the fly
+        pattern = "CONSTRUCT { " + pattern + "} WHERE { }";
+        // create a new Query object
+        Query query = QueryFactory.create(pattern);
+        // fetch triples
+        List<Triple> triples = query.getConstructTemplate().getTriples();
+        // feed the triples into the graph and bootstrap the class
+        Graph graph = ModelFactory.createDefaultModel().getGraph();
+        for (Triple t : triples) {
+            graph.add(t);
+        }
+
+        this.bootstrap(graph);
+    }
 
     public BGP(Graph bgpGraph) {
+        this.bootstrap(bgpGraph);
+    }
+
+    private void bootstrap(Graph bgpGraph) {
         // analyze the graph for variables
         Iterator<Triple> tripleIterator = bgpGraph.stream().iterator();
         int counter = 0;
@@ -26,11 +53,11 @@ public class BGP {
             this.triples.add(t);
 
             // variables can only show up in the subject or the object part of the triple
-            if (t.getSubject().getLiteral().toString().startsWith("?")) {
+            if (t.getSubject().isVariable()) {
                 this.subjectVariables.add(counter);
             }
 
-            if (t.getObject().getLiteral().toString().startsWith("?")) {
+            if (t.getObject().isVariable()) {
                 this.objectVariables.add(counter);
             }
             counter++;
@@ -41,7 +68,9 @@ public class BGP {
         // replace all known variables
         for (int index : this.subjectVariables) {
             Triple t = this.triples.get(index);
-            String variable = t.getSubject().getLiteralValue().toString();
+
+            String variable = "?" + t.getSubject().getName();
+
             if (m.containsKey(variable)) {
                 String value = m.get(variable).toString();
                 Triple newT = Triple.create(NodeFactory.createLiteral(value), t.getPredicate(), t.getObject());
@@ -51,7 +80,7 @@ public class BGP {
 
         for (int index : this.objectVariables) {
             Triple t = this.triples.get(index);
-            String variable = t.getObject().getLiteralValue().toString();
+            String variable = "?" + t.getObject().getName();
             if (m.containsKey(variable)) {
                 String value = m.get(variable).toString();
                 Triple newT = Triple.create(t.getSubject(), t.getPredicate(), NodeFactory.createLiteral(value));
--- src/test/java/be/ugent/idlab/knows/amo/blocks/BGPTest.java
+++ src/test/java/be/ugent/idlab/knows/amo/blocks/BGPTest.java
@@ -16,7 +16,7 @@ public class BGPTest {
     @Test
     public void simpleTest() {
         // create a simple input graph consisting of a single triple
-        Triple t = Triple.create(NodeFactory.createLiteral("?foo"), NodeFactory.createLiteral("foaf:name"), NodeFactory.createLiteral("?bar"));
+        Triple t = Triple.create(NodeFactory.createVariable("foo"), NodeFactory.createLiteral("foaf:name"), NodeFactory.createVariable("bar"));
         Graph graph = GraphFactory.createDefaultGraph();
         graph.add(t);
 
@@ -38,5 +38,38 @@ public class BGPTest {
 
         assertEquals("ex:JohnDoe", foundTriple.getSubject().getLiteralValue().toString());
         assertEquals("John", foundTriple.getObject().getLiteralValue().toString());
+    }
+
+    @Test
+    public void readGraphFromString() {
+        // graph as described in the paper Listing 2
+        String graph = "?firstname_iri <http://example.com/name> ?fullname;\n" +
+                "<http://example.com/petName> ?pet_name.";
+
+        // SolutionMapping with values as described in Example 9
+        SolutionMapping mapping = new SolutionMapping(Map.of(
+                "?firstname_iri", "<http://example.com/John>",
+                "?fullname", "John Doe",
+                "?pet_name", "Max"
+        ));
+
+        BGP bgp = new BGP(graph);
+
+        Graph out = bgp.apply(mapping);
+
+        assertEquals(2, out.size());
+        Iterator<Triple> iterator = out.find();
+        assertTrue(iterator.hasNext());
+        Triple t1 = iterator.next();
+        Triple t2 = iterator.next();
+        assertFalse(iterator.hasNext());
+
+        // check variable replacement in first triple
+        assertEquals("<http://example.com/John>", t1.getSubject().getLiteralValue().toString());
+        assertEquals("John Doe", t1.getObject().getLiteralValue());
+
+        // check variable replacement in second triple
+        assertEquals("<http://example.com/John>", t2.getSubject().getLiteralValue().toString());
+        assertEquals("Max", t2.getObject().getLiteralValue().toString());
     }
 }
