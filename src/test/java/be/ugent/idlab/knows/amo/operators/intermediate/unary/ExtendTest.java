63371f8de6c259f7f92be5ea30a01d8477935efb
Jozef Jankaj <jozef.jankaj@ugent.be>
2024-02-21 15:05:32 +0100

    chore: first version of BlocksIO usage in Extend tests

--- src/test/java/be/ugent/idlab/knows/amo/operators/intermediate/unary/ExtendTest.java
+++ src/test/java/be/ugent/idlab/knows/amo/operators/intermediate/unary/ExtendTest.java
@@ -3,6 +3,7 @@ package be.ugent.idlab.knows.amo.operators.intermediate.unary;
 import be.ugent.idlab.knows.amo.blocks.MappingTuple;
 import be.ugent.idlab.knows.amo.blocks.SolutionMapping;
 import be.ugent.idlab.knows.amo.operators.intermediate.unary.ExtendOperator;
+import be.ugent.idlab.knows.amo.utilities.BlocksIO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 
@@ -24,53 +25,19 @@ public class ExtendTest {
 
     @Test
     public void simpleTestSolMapping() {
-        SolutionMapping mapping = new SolutionMapping(
-                new HashMap<>() {{
-                    put("?first_name", "John");
-                    put("?last_name", "Doe");
-                    put("?age", 25);
-                }}
-        );
+        SolutionMapping input = BlocksIO.readSolutionMapping("src/test/resources/operators/extend/solutionMap/input.json");
+        SolutionMapping expected = BlocksIO.readSolutionMapping("src/test/resources/operators/extend/solutionMap/output.json");
+        SolutionMapping output = op.applySolMapping(input);
 
-        op.applySolMapping(mapping);
-
-        SolutionMapping expected = new SolutionMapping(
-                new HashMap<>() {{
-                    put("?first_name", "John");
-                    put("?last_name", "Doe");
-                    put("?full_name", "John Doe");
-                    put("?age", 25);
-                }});
-
-        assertEquals(expected, mapping);
+        assertEquals(expected, output);
     }
 
     @Test
     public void simpleTestMappingTuple() {
-        SolutionMapping solmap = new SolutionMapping(
-                new HashMap<>() {{
-                    put("?first_name", "John");
-                    put("?last_name", "Doe");
-                    put("?age", 25);
-                }}
-        );
-        MappingTuple tuple = new MappingTuple();
-        tuple.addSolutionMap("f_default", solmap);
+        MappingTuple input = BlocksIO.readMappingTuple("src/test/resources/operators/extend/mappingTuple/input.json");
+        MappingTuple expected = BlocksIO.readMappingTuple("src/test/resources/operators/extend/mappingTuple/output.json");
 
-        MappingTuple expected = new MappingTuple();
-        expected.addSolutionMap("f_default",
-                new SolutionMapping(
-                        Map.of(
-                                "?first_name", "John",
-                                "?last_name", "Doe",
-                                "?age", 25,
-                                "?full_name", "John Doe"
-                        )
-                )
-        );
-
-
-        MappingTuple actual = new MappingTuple();
-        actual.addSolutionMap("f_default", solmap);
+        MappingTuple actual = op.applyMappingTuple(input);
+        assertEquals(expected, actual);
     }
 }
