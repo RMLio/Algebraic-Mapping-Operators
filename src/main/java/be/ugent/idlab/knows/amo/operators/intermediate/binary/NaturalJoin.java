e8b7d014d6d5ed6f5efee8ee87c3e50b7f358fc5
Sitt Min Oo <sittminoo1995145@gmail.com>
2024-08-14 00:08:18 +0200

    feature: Implement natural join operator

--- src/main/java/be/ugent/idlab/knows/amo/operators/intermediate/binary/NaturalJoin.java
+++ src/main/java/be/ugent/idlab/knows/amo/operators/intermediate/binary/NaturalJoin.java
@@ -1,12 +1,16 @@
 package be.ugent.idlab.knows.amo.operators.intermediate.binary;
 
+import java.util.Set;
+
 import be.ugent.idlab.knows.amo.blocks.MappingTuple;
 import be.ugent.idlab.knows.amo.blocks.SolutionMapping;
 
 public class NaturalJoin extends Join {
 
     /**
-     * A convenience constructor where the input fragment is the same as the output fragment
+     * A convenience constructor where the input fragment is the same as the output
+     * fragment
+     * 
      * @param operatorName
      * @param inputFragment
      */
@@ -24,13 +28,32 @@ public class NaturalJoin extends Join {
 
     @Override
     public SolutionMapping apply(SolutionMapping mapping1, SolutionMapping mapping2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        if (mapping1.isCompatibleWith(mapping2)) {
+            return mapping1.union(mapping2);
+
+        }
+
+        return null;
+
     }
 
     @Override
     public MappingTuple apply(MappingTuple tuple1, MappingTuple tuple2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'apply'");
+        // TODO: Auto-generated method stub
+
+        MappingTuple result = new MappingTuple();
+
+        Set<String> commonFragments = tuple1.commonFragments(tuple2);
+
+        for (String fragment : commonFragments) {
+            for (SolutionMapping leftMapping : tuple1.getSolutionMappings(fragment)) {
+                for (SolutionMapping rightMapping : tuple2.getSolutionMappings(fragment)) {
+                    result.addSolutionMap(fragment, this.apply(leftMapping, rightMapping));
+                }
+            }
+        }
+
+        return result;
+
     }
 }
