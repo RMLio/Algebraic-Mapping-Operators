7843c28aaf3518239a03262b1bf47ce50671755f
Sitt Min Oo <sittminoo1995145@gmail.com>
2024-08-13 16:36:40 +0200

    chore: Update prettierrc and update docs for Pair class

--- src/main/java/be/ugent/idlab/knows/amo/blocks/Pair.java
+++ src/main/java/be/ugent/idlab/knows/amo/blocks/Pair.java
@@ -1,9 +1,9 @@
 package be.ugent.idlab.knows.amo.blocks;
 
 import java.io.Serializable; /**
- * A record Pair representing a tuple of two strings
- * @param first first string
- * @param second second string
+ * A record Pair representing a tuple of two elments
+ * @param first first element
+ * @param second second element
  */
 public record Pair<F, S> (F first, S second) implements Serializable  {
 }
