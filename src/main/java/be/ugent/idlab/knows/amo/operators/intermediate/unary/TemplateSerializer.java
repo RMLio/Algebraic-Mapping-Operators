e4bceeffe944fe721cc4a72e5349d60d7e5cfda2
Sitt Min Oo <sittminoo1995145@gmail.com>
2024-09-10 14:26:46 +0200

    fix: TemplateSerializer fills in variables in a template string correctly

--- src/main/java/be/ugent/idlab/knows/amo/operators/intermediate/unary/TemplateSerializer.java
+++ src/main/java/be/ugent/idlab/knows/amo/operators/intermediate/unary/TemplateSerializer.java
@@ -1,6 +1,15 @@
 package be.ugent.idlab.knows.amo.operators.intermediate.unary;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.simpleflatmapper.tuple.Tuple2;
 
 import be.ugent.idlab.knows.amo.blocks.SolutionMapping;
 import be.ugent.idlab.knows.amo.blocks.nodes.LiteralNode;
@@ -9,28 +18,58 @@ import be.ugent.idlab.knows.amo.blocks.nodes.RDFNode;
 public class TemplateSerializer extends UnaryOperator {
 
     private String serializedVariable;
-    private String template;
+    private List<Tuple2<Set<String>, String>> variablesTemplatePairs;
+    private Pattern variablePattern;
 
-    public TemplateSerializer(String operatorName, String fragment, String template) {
-        this(operatorName, fragment, template, "serialized_output");
+    public TemplateSerializer(String operatorName, String fragment, String templateString) {
+        this(operatorName, fragment, templateString, "serialized_output");
     }
 
-    public TemplateSerializer(String operatorName, String fragment, String template, String serializedVariable) {
+    public TemplateSerializer(String operatorName, String fragment, String templateString, String serializedVariable) {
         super(operatorName, fragment);
 
         this.serializedVariable = serializedVariable;
-        this.template = template;
+        this.variablePattern = Pattern.compile("\\?[a-zA-Z_0-9]+[^@^^]");
 
+        this.variablesTemplatePairs = this.extractTemplateVariables(templateString);
+
+    }
+
+    private List<Tuple2<Set<String>, String>> extractTemplateVariables(String template) {
+
+        List<Tuple2<Set<String>, String>> result = new ArrayList<>();
+        String[] newlinedTemplates = template.split("\n");
+
+        for (String line : newlinedTemplates) {
+            Matcher matcher = this.variablePattern.matcher(line);
+            Set<String> variables = new HashSet<>();
+            while (matcher.find()) {
+                variables.add(matcher.group().trim());
+            }
+            result.add(new Tuple2<Set<String>, String>(variables, line));
+        }
+
+        return result;
     }
 
     @Override
     SolutionMapping apply(SolutionMapping mapping) {
 
         SolutionMapping result = new SolutionMapping();
-        String serializedString = this.template;
-        for (Map.Entry<String, RDFNode> entry : mapping.entrySet()) {
-            serializedString.replaceAll("\\" + entry.getKey(), entry.getValue().toString());
+        List<String> serializedStringList = new ArrayList<>();
+        for (Tuple2<Set<String>, String> tuple : this.variablesTemplatePairs) {
+
+            String template = tuple.getElement1();
+            Set<String> variables = tuple.getElement0();
+
+            for (String variable : variables) {
+                template = template.replaceAll("\\" + variable, mapping.get(variable).toString());
+            }
+
+            serializedStringList.add(template);
         }
+
+        String serializedString = String.join("\n", serializedStringList);
 
         result.put(this.serializedVariable, new LiteralNode(serializedString));
         return result;
@@ -39,4 +78,9 @@ public class TemplateSerializer extends UnaryOperator {
     public String getSerializedVariable() {
         return serializedVariable;
     }
+
+    public List<Tuple2<Set<String>, String>> getVariablesTemplatePairs() {
+        return variablesTemplatePairs;
+    }
+
 }
--- src/test/java/be/ugent/idlab/knows/amo/operators/TemplateSerializerTest.java
+++ src/test/java/be/ugent/idlab/knows/amo/operators/TemplateSerializerTest.java
@@ -2,7 +2,14 @@ package be.ugent.idlab.knows.amo.operators;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+import org.simpleflatmapper.tuple.Tuple2;
 
 import be.ugent.idlab.knows.amo.blocks.MappingTuple;
 import be.ugent.idlab.knows.amo.blocks.SolutionMapping;
@@ -11,6 +18,29 @@ import be.ugent.idlab.knows.amo.blocks.nodes.LiteralNode;
 import be.ugent.idlab.knows.amo.operators.intermediate.unary.TemplateSerializer;
 
 public class TemplateSerializerTest {
+
+    @Test
+    public void variableExtractionTest() {
+        String template = """
+                ?sm ?pm_1 ?om@en.
+                ?sm <http://example.com/name/> ?om2^^<http://example.com/string>.
+                """;
+        TemplateSerializer serializer = new TemplateSerializer("Serializer", "default", template);
+
+        List<Tuple2<List<String>, String>> pairs = serializer.getVariablesTemplatePairs()
+                .stream()
+                .map((tuple) -> new Tuple2<List<String>, String>(
+                        tuple.getElement0().stream().sorted().collect(Collectors.toList()),
+                        tuple.getElement1()))
+                .collect(Collectors.toList());
+        List<Tuple2<List<String>, String>> expected = new ArrayList<>();
+        expected.add(new Tuple2<List<String>, String>(List.of("?om", "?pm_1", "?sm"), "?sm ?pm_1 ?om@en."));
+        expected.add(new Tuple2<List<String>, String>(List.of("?om2", "?sm"),
+                "?sm <http://example.com/name/> ?om2^^<http://example.com/string>."));
+
+        assertEquals(expected, pairs);
+
+    }
 
     @Test
     public void simpleTemplateSerialization() {
