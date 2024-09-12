c3b27e383166f3ba73429ee8d0bc0446363bae89
Sitt Min Oo <sittminoo1995145@gmail.com>
2024-09-12 16:36:28 +0200

    fix: Add nullable/nonnull annotations to solution mapping for nullness analysis

--- src/main/java/be/ugent/idlab/knows/amo/blocks/SolutionMapping.java
+++ src/main/java/be/ugent/idlab/knows/amo/blocks/SolutionMapping.java
@@ -7,6 +7,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -15,13 +18,13 @@ import com.google.gson.GsonBuilder;
  * their data
  * These can be applied on a templated string
  */
-public class SolutionMapping extends HashMap<String, RDFNode> {
+public class SolutionMapping extends HashMap<String, @Nullable RDFNode> {
     /**
      * A copy constructor
      *
      * @param mapping SolutionMapping to be copied
      */
-    public SolutionMapping(SolutionMapping mapping) {
+    public SolutionMapping(@NonNull SolutionMapping mapping) {
         this.putAll(mapping);
     }
 
@@ -29,16 +32,31 @@ public class SolutionMapping extends HashMap<String, RDFNode> {
         super();
     }
 
-    public SolutionMapping(Map<String, RDFNode> variables) {
-        this.putAll(variables);
+    public SolutionMapping(@NonNull Map<String, @Nullable RDFNode> solutionMapping) {
+        this.putAll(solutionMapping);
     }
 
     /**
      * @param that
      */
-    public SolutionMapping union(SolutionMapping that) {
+    public SolutionMapping union(@Nullable SolutionMapping that) {
+        if (that == null) {
+            return new SolutionMapping(this);
+        }
         SolutionMapping sol = new SolutionMapping(this);
         sol.putAll(that);
+        // find all common keys
+        Set<String> commonKeys = new HashSet<>(this.keySet());
+        commonKeys.retainAll(sol.keySet());
+
+        for (String key : commonKeys) {
+            RDFNode thisValue = this.get(key);
+            RDFNode solValue = sol.get(key);
+            if (thisValue != null && solValue == null) {
+                sol.put(key, thisValue);
+            }
+        }
+
         return sol;
     }
 
@@ -51,7 +69,11 @@ public class SolutionMapping extends HashMap<String, RDFNode> {
      * @param that SolutionMapping to check compatibility with
      * @return true if the SolutionMappings are compatible, false otherwise
      */
-    public boolean isCompatibleWith(SolutionMapping that) {
+    public boolean isCompatibleWith(@Nullable SolutionMapping that) {
+        if (that == null) {
+            return true;
+        }
+
         if (this.isEmpty() || that.isEmpty()) {
             return true;
         }
@@ -66,8 +88,13 @@ public class SolutionMapping extends HashMap<String, RDFNode> {
 
         // verify the values of all common keys
         for (String key : commonKeys) {
-            if (!this.get(key).equals(that.get(key))) {
-                return false;
+            RDFNode thisValue = this.get(key);
+            RDFNode thatValue = that.get(key);
+
+            if (thisValue != null && thatValue != null) {
+                if (!thisValue.equals(thatValue)) {
+                    return false;
+                }
             }
         }
         return true;
